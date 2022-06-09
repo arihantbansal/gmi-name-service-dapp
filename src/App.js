7e9e4f4f23cca4dccb4dd232c32b812daf499f09
arihantbansal <17180950+arihantbansal@users.noreply.github.com>
2022-06-09 22:58:40 +0530

    :construction: working on it

--- src/App.js
+++ src/App.js
@@ -1,12 +1,115 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./styles/App.css";
 import twitterLogo from "./assets/twitter-logo.svg";
 
-// Constants
 const TWITTER_HANDLE = "_arihantbansal";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
+const tld = ".gmi";
+const CONTRACT_ADDRESS = "0x934333c64FaCa3bFA87fB7a9863dc18A4D6e5B3C";
+
 const App = () => {
+	const [currentAccount, setCurrentAccount] = useState("");
+	const [domain, setDomain] = useState("");
+	const [record, setRecord] = useState("");
+
+	const connectWallet = async () => {
+		try {
+			const { ethereum } = window;
+
+			if (!ethereum) {
+				alert("Get MetaMask -> https://metamask.io/");
+				return;
+			}
+
+			const accounts = await ethereum.request({
+				method: "eth_requestAccounts",
+			});
+
+			console.log("Connected", accounts[0]);
+			setCurrentAccount(accounts[0]);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	const checkIfWalletIsConnected = async () => {
+		const { ethereum } = window;
+
+		if (!ethereum) {
+			console.log("Make sure you have MetaMask!");
+			return;
+		} else {
+			console.log("We have the ethereum object", ethereum);
+		}
+
+		const accounts = await ethereum.request({ method: "eth_accounts" });
+
+		if (accounts.length !== 0) {
+			const account = accounts[0];
+			console.log("Found an authorized account:", account);
+			setCurrentAccount(account);
+		} else {
+			console.log("No authorized account found");
+		}
+	};
+
+	const renderNotConnectedContainer = () => (
+		<div className="connect-wallet-container">
+			<img
+				src="https://media.giphy.com/media/CSbIZi52DvqnJPm1WA/giphy.gif"
+				alt="unicorn gif"
+			/>
+			<button
+				onClick={connectWallet}
+				className="cta-button connect-wallet-button">
+				Connect Wallet
+			</button>
+		</div>
+	);
+
+	const renderInputForm = () => {
+		return (
+			<div className="form-container">
+				<div className="first-row">
+					<input
+						type="text"
+						value={domain}
+						placeholder="domain"
+						onChange={(e) => setDomain(e.target.value)}
+					/>
+					<p className="tld"> {tld} </p>
+				</div>
+
+				<input
+					type="text"
+					value={record}
+					placeholder="whats ur ninja power"
+					onChange={(e) => setRecord(e.target.value)}
+				/>
+
+				<div className="button-container">
+					<button
+						className="cta-button mint-button"
+						disabled={null}
+						onClick={null}>
+						Mint
+					</button>
+					<button
+						className="cta-button mint-button"
+						disabled={null}
+						onClick={null}>
+						Set data
+					</button>
+				</div>
+			</div>
+		);
+	};
+
+	useEffect(() => {
+		checkIfWalletIsConnected();
+	}, []);
+
 	return (
 		<div className="App">
 			<div className="container">
@@ -18,6 +121,9 @@ const App = () => {
 						</div>
 					</header>
 				</div>
+
+				{!currentAccount && renderNotConnectedContainer()}
+				{currentAccount && renderInputForm()}
 
 				<div className="footer-container">
 					<img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
